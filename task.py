4b8d7e36987fdff14d7b9dc4ab9dce8cf73de0e9
Archie <76575196+archiewang0@users.noreply.github.com>
2025-01-31 00:54:56 +0800

    updat task.py

--- task.py
+++ task.py
@@ -1,1 +1,103 @@
-print('Hello World')
+
+# \n 字串斷行
+# print('字串斷行: Hello World\nHello World\n')
+# # 字串相加
+# print('字串相加: Hello'+'World\n')
+
+# # age = input("how old are you?")
+# # print('i am '+ age)
+
+
+# len('test')
+# # 4
+
+# print("hello"[2])
+
+# # 列印數字
+# print(12312312)
+
+# # 列印large integ
+# print(123_12_32_3_5)
+
+# print(3.12321313)
+
+# print(type("hello"))
+# print(type(123))
+# print(type(123_456_677_777_565))
+# print(type(3.14159))
+
+# print( int('123') + int('321'))
+
+# # print('fsdfdsfsfsf' + str(len(input('enter?'))))
+
+# print(3-7)
+# print(type(3-7))
+# print(3+3)
+# print(3/3)
+# print(type(3/3))
+# print(4//3)
+# print(type(4//3)) #取整數
+# print(3*2)
+# print(3**2) #平方
+
+#運算運作順序
+# () 
+# ** 
+# / OR *
+# + OR -
+# print(int(30.89))
+# print(round(30.89))
+# print(round(30.89 , 1)) #小數點後幾位
+
+# number_var = 23132
+bool_var = False
+# float_var = 0.421
+# print('number: '+ str(number_var))
+
+# # f"" 內使用{} 可以帶入任何變數
+# print(f"number: {number_var}")
+# print(f"boolean: {bool_var}")
+# print(f"float: {float_var}")
+
+# import math
+
+# total = input("總共金額多少錢?")
+# split_people = input("多少人分?")
+# print(f"總共: { math.ceil(int(total)/ int(split_people))}")
+
+
+# if bool_var:
+#     print('true')
+# else:
+#     print('false')
+
+# 取餘數
+# print(100 % 2)
+
+# height = int(input('how tall you are?'))
+# age = int(input('your age? '))
+# wanna_take_poto = input('wanna take poto? Yes(y) , No(n)')
+
+# if height > 160:
+#     bill = 0
+
+#     if wanna_take_poto == 'y':
+#         bill = 50
+
+#     if age > 16:
+#         bill = bill + 360
+#         print(f"tickt {bill}")
+#     elif age < 16 and age >= 10:
+#         bill = bill + 280
+#         print(f"tickt {bill}")
+#     else:
+#         bill = bill + 0
+#         if bill == 0:
+#             print('free tickt')
+#         else:
+#             print(f"price {bill}")
+# else:
+#     print('soory , you too Short ! not allow to play this .')
+
+
+# not只能用在 if else condition中
